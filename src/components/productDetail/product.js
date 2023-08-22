3e1d0297896f61a54812fbe5eec940222533c021
ArshdeepSingh10 <114170992+ArshdeepSingh10@users.noreply.github.com>
2023-08-22 19:06:14 +0530

    Update product.js

--- src/components/productDetail/product.js
+++ src/components/productDetail/product.js
@@ -103,33 +103,14 @@ const [cartVisibilities, setCartVisibilities] = useState(carts.map(() => false))
 <h1></h1>
 </div>
 <div className='flex pe-32 py-4 gap-5'>
-  {!product.image ? (
-    <div class="text-center">
-      <div role="status">
-        <svg
-          aria-hidden="true"
-          class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-          viewBox="0 0 100 101"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          {/* ... SVG Path data ... */}
-        </svg>
-        <span class="sr-only">Loading...</span>
-      </div>
+    {!product.image ? " ":  product.image.map((image , index) =>(
+        <div key={index}>
+        <img src={image} onClick={()=>(changeimage(image))}  className='object-fill' />
     </div>
-  ) : (
-    product.image.map((image, index) => (
-      <div key={index}>
-        <img src={image} onClick={() => changeimage(image)} className='object-fill' />
-      </div>
-    ))
-  )}
-</div>
 
     ))}
     
-</div>
+
       </div>
       {/*------------------------------------------- product detail ----------------(----------- */}
       <div className='col-span-12 md:col-span-6 '>
