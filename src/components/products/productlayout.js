2408a830884143ff4b7611b9a3975e48cc7170e9
ArshdeepSingh10 <114170992+ArshdeepSingh10@users.noreply.github.com>
2023-08-22 17:52:43 +0530

    Update productlayout.js

--- src/components/products/productlayout.js
+++ src/components/products/productlayout.js
@@ -373,11 +373,11 @@ const Productlayout = () => {
             {/*--------------------------------------- main ------------------------------------*/}
             <div className="grid md:grid-cols-4 grid-cols-2 gap-4 p-2">
               {/* --------------------------------------------------------card-------------------------------------------------  */}
-              {products.length > 0 ? (
+              {/*products.length > 0 ? (*/
         show
-      ) : (
+     /* ) : (
         <div>No products found for the selected category.</div>
-      )}
+      )}*/
 
                
               {/* end  */}
